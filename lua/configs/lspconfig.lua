7f2c812d3a0bf6fc5714e8dbe7a1ae14dc5a7859
Raphael-Soares <soaresphaell@gmail.com>
2024-12-18 09:10:07 -0300

    fix: better suport for vue

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -10,7 +10,7 @@ lspconfig.servers = {
     "cssls",
 }
 
-local default_servers = { "html", "cssls", "volar" }
+local default_servers = { "html", "cssls" }
 for _, lsp in ipairs(default_servers) do
     lspconfig[lsp].setup({
         on_attach = nvlsp.on_attach,
@@ -22,6 +22,24 @@ end
 local vue_typescript_plugin = require("mason-registry").get_package("vue-language-server"):get_install_path()
     .. "/node_modules/@vue/language-server"
     .. "/node_modules/@vue/typescript-plugin"
+
+lspconfig.volar.setup({
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+    filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+    init_options = {
+        typescript = {
+            tsdk = vim.fn.stdpath("data") .. "/mason/packages/typescript-language-server/node_modules/typescript/lib",
+        },
+        languageFeatures = {
+            completion = {
+                defaultTagNameCase = "both",
+                defaultAttrNameCase = "kebabCase",
+            },
+        },
+    },
+})
 
 lspconfig.ts_ls.setup({
     on_attach = nvlsp.on_attach,
