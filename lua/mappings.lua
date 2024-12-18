2433afa3f13b34aa6b5c8fd24fa21ed1aedb8f37
Raphael-Soares <soaresphaell@gmail.com>
2024-12-18 08:51:18 -0300

    docs: added comments to mappings file

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,77 +7,80 @@ local terminal = require("nvchad.term")
 -- Disable mappings
 local nomap = vim.keymap.del
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
+-- General mappings
+map("n", ";", ":", { desc = "Enter command mode" })
+map({ "i", "x", "n", "s" }, "<c-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+map("n", "+", "<c-a>", { desc = "Increment number" })
+map("n", "-", "<c-x>", { desc = "Decrement number" })
+map("n", "<c-a>", "gg<s-v>g", { desc = "Select all" })
+map("n", "<c-d>", "<c-d>zz", { desc = "Scroll down and center" })
+map("n", "<c-u>", "<c-u>zz", { desc = "Scroll up and center" })
 
-map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+-- NvimTree toggle
+map("n", "<leader>e", "<cmd>nvimtreetoggle<cr>", { desc = "Toggle NvimTree window" })
 
-map("n", "+", "<C-a>", { desc = "Increment Number" })
-map("n", "-", "<C-x>", { desc = "Decrement Number" })
+-- Window navigation
+map("n", "<c-h>", "<c-w>h", { desc = "Switch window left" })
+map("n", "<c-l>", "<c-w>l", { desc = "Switch window right" })
+map("n", "<c-j>", "<c-w>j", { desc = "Switch window down" })
+map("n", "<c-k>", "<c-w>k", { desc = "Switch window up" })
 
-map("n", "<C-a>", "gg<S-v>G", { desc = "Select All" })
+-- Line movement
+map("n", "<a-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move line down" })
+map("n", "<a-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move line up" })
+map("i", "<a-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move line down in insert mode" })
+map("i", "<a-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move line up in insert mode" })
+map("v", "<a-j>", ":<c-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move selection down" })
+map("v", "<a-k>", ":<c-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move selection up" })
 
-map("n", "<C-d>", "<C-d>zz", { desc = "Scroll Down and Center" })
-map("n", "<C-u>", "<C-u>zz", { desc = "Scroll Up and Center" })
+-- Window management
+map("n", "<leader>sv", "<c-w>s", { desc = "Split window below", remap = true })
+map("n", "<leader>sb", "<c-w>v", { desc = "Split window right", remap = true })
+map("n", "<leader>sd", "<c-w>c", { desc = "Delete window", remap = true })
 
-map("n", "<C-j>", "<C-d>zz", { desc = "Scroll Down and Center" })
-map("n", "<C-k>", "<C-u>zz", { desc = "Scroll Up and Center" })
-
-map("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Line Down" })
-map("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Line Up" })
-map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Line Down in Insert Mode" })
-map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Line Up in Insert Mode" })
-map("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Selection Down" })
-map("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Selection Up" })
-
-map("n", "<S-l>", function()
-    require("nvchad.tabufline").next()
-end, { desc = "Previous Buffer" })
-map("n", "<S-h>", function()
-    require("nvchad.tabufline").prev()
-end, { desc = "Next Buffer" })
-
-map("n", "<leader>bn", "<cmd>enew<CR>", { desc = "New buffer" })
-
-map("n", "<leader>bD", "<cmd>:bd<cr>", { desc = "Delete Buffer and Window" })
-
+-- Buffer navigation
+map("n", "<s-l>", function()
+    buffer.next()
+end, { desc = "Next buffer" })
+map("n", "<s-h>", function()
+    buffer.prev()
+end, { desc = "Previous buffer" })
+map("n", "<leader>bn", "<cmd>enew<cr>", { desc = "New buffer" })
 map("n", "<leader>bd", function()
     buffer.close_buffer()
-end, { desc = "Delete Buffer" })
-
+end, { desc = "Delete buffer" })
 map("n", "<leader>bo", function()
-    buffer.closeBufs_at_direction("left")
-    buffer.closeBufs_at_direction("right")
-end, { desc = "Delete Other Buffers" })
-
+    buffer.closebufs_at_direction("left")
+    buffer.closebufs_at_direction("right")
+end, { desc = "Delete other buffers" })
 map("n", "<leader>ba", function()
-    buffer.closeAllBufs()
-end, { desc = "Delete All Buffers" })
+    buffer.closeallbufs()
+end, { desc = "Delete all buffers" })
 
+-- Terminal management
 map("n", "<leader>h", function()
     terminal.new({ pos = "sp" })
-end, { desc = "terminal new horizontal term" })
-
+end, { desc = "New horizontal terminal" })
 map("n", "<leader>v", function()
     terminal.new({ pos = "vsp" })
-end, { desc = "terminal new vertical term" })
+end, { desc = "New vertical terminal" })
 
--- toggleable terminal
-map({ "n", "t" }, "<A-v>", function()
-    terminal.toggle({ pos = "vsp", id = "vtoggleTerm" })
-end, { desc = "terminal toggleable vertical term" })
+-- Toggleable terminals
+map({ "n", "t" }, "<a-v>", function()
+    terminal.toggle({ pos = "vsp", id = "vtoggleterm" })
+end, { desc = "Toggle vertical terminal" })
+map({ "n", "t" }, "<a-h>", function()
+    terminal.toggle({ pos = "sp", id = "htoggleterm" })
+end, { desc = "Toggle horizontal terminal" })
+map({ "n", "t" }, "<c-_>", function()
+    terminal.toggle({ pos = "sp", id = "htoggleterm" })
+end, { desc = "Toggle horizontal terminal" })
+map({ "n", "t" }, "<a-f>", function()
+    terminal.toggle({ pos = "float", id = "floatterm" })
+end, { desc = "Toggle floating terminal" })
 
-map({ "n", "t" }, "<A-h>", function()
-    terminal.toggle({ pos = "sp", id = "htoggleTerm" })
-end, { desc = "terminal toggleable horizontal term" })
+-- Quit command
+map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
 
-map({ "n", "t" }, "<C-_>", function()
-    terminal.toggle({ pos = "sp", id = "htoggleTerm" })
-end, { desc = "terminal toggleable horizontal term" })
-
-map({ "n", "t" }, "<A-f>", function()
-    terminal.toggle({ pos = "float", id = "floatTerm" })
-end, { desc = "terminal toggle floating term" })
-
-map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
-
+-- Disable specific mappings
 nomap("n", "<leader>bn")
