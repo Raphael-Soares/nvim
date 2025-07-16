425497e4e8d22e404f36ea6d616fba166013a1a7
Raphael Soares <soaresphaell@gmail.com>
2025-07-16 09:36:39 -0300

    feat: new lsp config

--- lua/plugins/lspconfig/init.lua
+++ lua/plugins/lspconfig/init.lua
@@ -3,30 +3,35 @@ return {
     config = function()
         require("nvchad.configs.lspconfig").defaults()
 
-        local lspconfig = require("lspconfig")
         local nvlsp = require("nvchad.configs.lspconfig")
         local keymaps = require("plugins.lspconfig.keymaps")
 
-        lspconfig.servers = {
+        local servers = {
             "lua_ls",
             "html",
             "cssls",
+            "clangd",
+            "pyright",
             "tailwindcss",
+            "vtsls",
             "vue_ls",
-            "ts_ls",
         }
 
-        local default_servers = { "html", "tailwindcss", "cssls" }
-        for _, lsp in ipairs(default_servers) do
-            lspconfig[lsp].setup({
+        for _, lsp in ipairs(servers) do
+            vim.lsp.config(lsp, {
                 on_attach = keymaps.on_attach,
                 on_init = nvlsp.on_init,
                 capabilities = nvlsp.capabilities,
             })
         end
 
-        require("plugins.lspconfig.lang.typescript").setup()
-        require("plugins.lspconfig.lang.python").setup()
-        require("plugins.lspconfig.lang.clang").setup()
+        local vue_configs = require("plugins.lspconfig.vue")
+        local vtsls = vue_configs.vtsls()
+        local vue_ls = vue_configs.vue_ls()
+
+        vim.lsp.config("vue_ls", vue_ls)
+        vim.lsp.config("vtsls", vtsls)
+
+        vim.lsp.enable(servers)
     end,
 }
